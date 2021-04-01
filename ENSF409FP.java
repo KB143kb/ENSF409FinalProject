9269746f73472f7b06ff05506efe510bdbbc8c2c
KB143kb <71091678+KB143kb@users.noreply.github.com>
2021-04-01 14:35:17 -0600

    Create ENSF409FP.java

---
+++ ENSF409FP.java
@@ -0,0 +1,30 @@
+package ENSF409;
+
+import java.sql.*;
+
+public class ENSF409FP 
+{
+	private Connection dbConnect;
+	private ResultSet results;
+	
+	public ENSF409FP () {}
+	
+	public void createConnection ()
+	{
+		try
+		{
+			dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/inventory", "ENSF409", "ensf409");
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
