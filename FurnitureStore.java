7f3cb7f3f86d1ec1b0f156f9175ea5f8484447e6
KB143kb <71091678+KB143kb@users.noreply.github.com>
2021-04-04 18:25:51 -0600

    Create FurnitureStore.java

---
+++ FurnitureStore.java
@@ -0,0 +1,18 @@
+package edu.ucalgary.ensf409;
+
+public class FurnitureStore 
+{
+	private String DBURL;
+	private String USERNAME;
+	private String PASSWORD;
+	
+		public int calculateLowestPrice (String [][] inventoryTable)
+		{
+			
+		}
+		
+		public void main (String [] arg)
+		{
+			
+		}
+}
