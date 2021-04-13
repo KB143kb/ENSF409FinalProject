d48d4bd4422be033313b921bd6943dec148dbde1
KB143kb <71091678+KB143kb@users.noreply.github.com>
2021-04-13 17:56:29 -0600

    Delete FurnitureNotAvailableException.java

--- edu/ucalgary/ensf409/FurnitureNotAvailableException.java
+++
@@ -1,5 +0,0 @@
-public class FurnitureNotAvailableException extends Exception {
-	public FurnitureNotAvailableException () {
-		super("The furniture type you want to order is not available");
-	}
-}
